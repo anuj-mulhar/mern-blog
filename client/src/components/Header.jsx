cbdfc28ad1e87e4961b9c6f0c8bca1f89b473877
anuj-mulhar <mulharanujsuresh123@gmail.com>
2024-04-04 19:04:52 +0530

    Create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
         </Button>
 
         <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue'>
+          <Button gradientDuoTone='purpleToBlue' outline >
             Sign In
           </Button>
         </Link>
