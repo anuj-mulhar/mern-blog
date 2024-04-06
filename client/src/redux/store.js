a34b8882401cbb2b433ca1c2474864f465a30379
anuj-mulhar <mulharanujsuresh123@gmail.com>
2024-04-06 14:49:31 +0530

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice.js'
+import { configureStore , combineReducers} from '@reduxjs/toolkit';
+import userReducer from './user/userSlice.js';
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { version } from 'mongoose';
+import persistStore from 'redux-persist/es/persistStore.js';
+
+const rootReducer = combineReducers({
+    user: userReducer,
+});
+const persistConfig = {
+    key:'root',
+    storage,
+    version: 1,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user : userReducer,
-  },
+  reducer:persistedReducer,
+  middleware: (getDefaultMiddleware) => 
+  getDefaultMiddleware({serializableCheck: false}),
 });
+
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 // export type RootState = ReturnType<typeof store.getState>
