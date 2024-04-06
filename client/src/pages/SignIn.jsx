839cff014703c1c9041cf5aed09d7ea5541e7ede
anuj-mulhar <mulharanujsuresh123@gmail.com>
2024-04-06 13:56:14 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,13 +1,15 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import { signInSuccess,signInFailure,signInStart } from '../redux/user/userSlice.js';
 
 
 
 export default function SignIn() {
   const [formData, setFormData, ] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const {loading, error: errorMessage} = useSelector(state => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim()});
@@ -15,11 +17,10 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if(!formData.email || !formData.password){
-      return setErrorMessage('Please fill out all fields.');
+      return dispatch(signInFailure('Please fill all the fields'));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/sign-in',{
         method: "POST",
         headers: { 'Content-Type':'application/json'},
@@ -27,18 +28,16 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
       if(res.ok){
+        dispatch(signInSuccess(data));
         navigate('/');
       }
     } catch (error) {
-      setErrorMessage(error.message);
+      dispatch(signInFailure(error.message));
     }
   };
-
-
   return (
     <div className='min-h-screen mt-20' >
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5' >
@@ -87,13 +86,11 @@ export default function SignIn() {
             </span>
             <Link to='/signup' className='text-blue-500'>Sign Up</Link>
           </div>
-          {
-            errorMessage && (
+          {errorMessage && (
               <Alert className='mt-5' color='failure'>
                 {errorMessage}
               </Alert>
-            )
-          }
+            )}
         </div>
         </div>  
     </div>
