7167bfad7ce84ddd09e66a625c207e46531874ba
anuj-mulhar <mulharanujsuresh123@gmail.com>
2024-04-10 23:11:44 +0530

    set route for the admin

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -4,7 +4,7 @@ import { google, signin, signup } from '../controllers/auth.controller.js';
 const router = express.Router();
 
 router.post('/signup', signup);
-router.post('/sign-in', signin);
+router.post('/signin', signin);
 router.post('/google', google);
 
 
