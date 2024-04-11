382ea9f052a3ba14bf21c539a216afba0cb05057
anuj-mulhar <mulharanujsuresh123@gmail.com>
2024-04-11 19:10:53 +0530

    add create page functionality through  dashboard

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -20,10 +20,11 @@ import {
   signoutSuccess,
 } from "../redux/user/userSlice.js";
 import { useDispatch } from "react-redux";
-import {HiOutlineExclamationCircle} from 'react-icons/hi';
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+import {Link } from 'react-router-dom';
 
 export default function DashProfile() {
-  const { currentUser, error } = useSelector((state) => state.user);
+  const { currentUser, error, loading } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -114,24 +115,26 @@ export default function DashProfile() {
         setUpdateUserError(data.message);
       } else {
         dispatch(updateSuccess(data));
-        setUpdateUserSuccess("User's profile updated successfully ! Refresh page to see the changes");
+        setUpdateUserSuccess(
+          "User's profile updated successfully ! Refresh page to see the changes"
+        );
       }
     } catch (error) {
       dispatch(updateFailure(error.message));
       setUpdateUserError(error.message);
     }
   };
-  const handleDeleteUser = async() => {
+  const handleDeleteUser = async () => {
     setShowModal(false);
     try {
       dispatch(deleteUserStart());
-      const res = await fetch(`/api/user/delete/${currentUser._id}`,{
-        method: 'DELETE',
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
       });
       const data = await res.json();
-      if(!res.ok) {
+      if (!res.ok) {
         dispatch(deleteUserFailure(data.message));
-      } else{
+      } else {
         dispatch(deleteUserSuccess(data));
       }
     } catch (error) {
@@ -139,15 +142,15 @@ export default function DashProfile() {
     }
   };
 
-  const handleSignout = async() => {
+  const handleSignout = async () => {
     try {
-      const res = await fetch('/api/user/signout',{
-        method: 'POST',
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
       });
       const data = await res.json();
-      if(!res.ok) {
+      if (!res.ok) {
         console.log(data.message);
-      }else{
+      } else {
         dispatch(signoutSuccess());
       }
     } catch (error) {
@@ -221,42 +224,68 @@ export default function DashProfile() {
           placeholder="password"
           onChange={handleChange}
         />
-        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
-          Update
+        <Button type="submit" gradientDuoTone="purpleToBlue" outline disabled={loading || imageFileUploading}>
+          {loading ? 'Loading...' : 'Update'}
         </Button>
+        {currentUser.isAdmin && (
+          <Link to={'/create-post'} >
+            <Button
+              type="button"
+              gradientDuoTone="purpleToPink"
+              className="w-full"
+            >
+              Create a post
+            </Button>
+          </Link>
+        )}
       </form>
       <div className="text-red-500 flex justify-between mt-5 ">
-        <span onClick={()=>setShowModal(true)} className="cursor-pointer ">Delete Account </span>
-        <span onClick={handleSignout} className="cursor-pointer ">Sign Out </span>
+        <span onClick={() => setShowModal(true)} className="cursor-pointer ">
+          Delete Account{" "}
+        </span>
+        <span onClick={handleSignout} className="cursor-pointer ">
+          Sign Out{" "}
+        </span>
       </div>
       {updateUserSuccess && (
-        <Alert color='success' className='mt-5'>
-        {updateUserSuccess}
+        <Alert color="success" className="mt-5">
+          {updateUserSuccess}
         </Alert>
       )}
       {updateUserError && (
-        <Alert color='failure' className="mt-5">
+        <Alert color="failure" className="mt-5">
           {updateUserError}
         </Alert>
       )}
       {error && (
-        <Alert color='failure' className="mt-5">
+        <Alert color="failure" className="mt-5">
           {error}
         </Alert>
       )}
-      <Modal show={showModal} onClose={() => setShowModal(false)} popup size='md'>
-        <Modal.Header/>
-      
-        <Modal.Body> 
+      <Modal
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        popup
+        size="md"
+      >
+        <Modal.Header />
+
+        <Modal.Body>
           <div className="text-center">
-            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-400 mb-4 mx-auto"/>
-            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">Are you sure you want to delete your account ?</h3>
+            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-400 mb-4 mx-auto" />
+            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
+              Are you sure you want to delete your account ?
+            </h3>
             <div className="flex justify-center gap-4">
-              <Button color='failure' onClick={handleDeleteUser}>Yes, I'm sure</Button>
-              <Button color='gray' onClick={()=> setShowModal(false)}>No, Cancel</Button>
+              <Button color="failure" onClick={handleDeleteUser}>
+                Yes, I'm sure
+              </Button>
+              <Button color="gray" onClick={() => setShowModal(false)}>
+                No, Cancel
+              </Button>
             </div>
           </div>
-        </Modal.Body> 
+        </Modal.Body>
       </Modal>
     </div>
   );
