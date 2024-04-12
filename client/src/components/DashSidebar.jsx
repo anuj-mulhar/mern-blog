b5879330a6d7dd68bdea4aa1217c6e8b5646873a
anuj-mulhar <mulharanujsuresh123@gmail.com>
2024-04-12 23:46:19 +0530

    Added posts section to the dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,51 +1,73 @@
-import React, { useEffect, useState } from 'react';
-import {Link, useLocation} from 'react-router-dom';
-import {Sidebar} from 'flowbite-react';
-import {HiArrowSmRight, HiUser} from 'react-icons/hi';
-import {useDispatch} from  "react-redux";
-import { signoutSuccess } from '../redux/user/userSlice';
-
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { Sidebar } from "flowbite-react";
+import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi";
+import { useDispatch } from "react-redux";
+import { signoutSuccess } from "../redux/user/userSlice";
+import { useSelector } from "react-redux"; 
 
 export default function DashSidebar() {
-    const location = useLocation();
-    const dispatch = useDispatch();
-    const [tab, setTab] = useState('');
-    useEffect(()=>{
-      const urlParams = new URLSearchParams(location.search);
-      const tabFromUrl = urlParams.get('tab');
-      if (tabFromUrl) {
-        setTab(tabFromUrl);
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const {currentUser} = useSelector((state) => state.user);
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  const handleSignout = async () => {
+    try {
+      const res = await fetch("/api/user/signout", {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
       }
-    }, [location.search]);
-    const handleSignout = async() => {
-      try {
-        const res = await fetch('/api/user/signout',{
-          method: 'POST',
-        });
-        const data = await res.json();
-        if(!res.ok) {
-          console.log(data.message);
-        }else{
-          dispatch(signoutSuccess());
-        }
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   return (
-    <Sidebar className='w-full md:w-56'>
-        <Sidebar.Items>
-            <Sidebar.ItemGroup>
-                <Link to='/dashboard?tab=profile'>
-                <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"User"} labelColor='dark' as ='div'>
-                    Profile
-                </Sidebar.Item>
-                </Link>
-                <Sidebar.Item  icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout} >
-                    Sign Out
-                </Sidebar.Item>
-            </Sidebar.ItemGroup>
-        </Sidebar.Items>
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
+        <Sidebar.ItemGroup className="flex flex-col gap-1">
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={currentUser.isAdmin ? 'Admin' : 'User'}
+              labelColor="dark"
+              as="div"
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          {currentUser.isAdmin && (
+            <Link to="/dashboard?tab=posts">
+              <Sidebar.Item
+                active={tab == "posts"}
+                icon={HiDocumentText}
+                as="div"
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
+          )}
+          <Sidebar.Item
+            icon={HiArrowSmRight}
+            className="cursor-pointer"
+            onClick={handleSignout}
+          >
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
     </Sidebar>
-  )
+  );
 }
