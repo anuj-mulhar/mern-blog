a6681965a63e6f2fc0cd5519f8642f2394b84a78
anuj-mulhar <mulharanujsuresh123@gmail.com>
2024-04-12 23:47:09 +0530

    added admin and user as per condition

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import {useLocation} from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
+import DashPosts from '../components/DashPosts';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -21,6 +22,9 @@ export default function Dashboard() {
       </div>
         { /*Profile... */ }
         {tab === 'profile' && <DashProfile/>}
+        { /*Posts... */ }
+        {tab === 'posts' && <DashPosts />}
+
     </div>
   );
 }
