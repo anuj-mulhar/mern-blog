5afb7aac7d2d93ab529a5c32f529fc394fd05683
anuj-mulhar <mulharanujsuresh123@gmail.com>
2024-04-15 00:43:37 +0530

    add delete user functionality to the admin user

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -9,7 +9,7 @@ export default function DashUsers() {
   const [users, setUsers] = useState([]);
   const [showMore, setShowMore] = useState(true);
   const [showModal, setShowModal] = useState(false);
-  const [postIdToDelete, setPostIdToDelete] = useState('');
+  const [userIdToDelete, setUserIdToDelete] = useState('');
   useEffect(() => {
     const fetchUsers = async () => {
       try {
@@ -48,6 +48,21 @@ export default function DashUsers() {
     }
   };
   const handleDeleteUser = async () => {
+    try {
+        const res = await fetch(`/api/user/delete/${userIdToDelete}`,{
+            method: 'DELETE',
+        });
+        const data =await res.json();
+        if (res.ok){
+            setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete))
+            setShowModal(false);}
+            else{
+                console.log(data.message);
+            }
+        } catch (error) {
+        console.log(data.message);
+        
+    }
   };
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500 ">
